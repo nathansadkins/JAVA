bd2046e93317bac6c4cc3b979977ee8e823a1fbc
nathansadkins <42559068+nathansadkins@users.noreply.github.com>
2019-11-14 22:19:03 -0600

    Add files via upload

---
+++ Activity 1/StudentInfo.java
@@ -0,0 +1,21 @@
+/**
+*This program outputs user information such as 
+*name and previous computer experience.
+*
+*Activity 01.
+*Nathan Adkins - COMP-1210-001. 
+*8-26-19.
+*/
+public class StudentInfo 
+{
+ /**
+   *Prints name and previous computer experience.
+   *@param args Command line arguments - not used. 
+   */
+   public static void main(String[] args)
+   {
+      System.out.println("Name: Nathan Adkins");
+      System.out.println("Previous Computer Classes:");
+      System.out.println("   Matlab");
+   }
+}
